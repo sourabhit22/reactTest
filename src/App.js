71c6715954e788b9776f2d0626527c4795527ef9
sourabhit22 <sourabh.srbit@gmail.com>
2023-05-17 15:21:31 +0530

    basic react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {User, User2} from './User';
+import About from './About';
+
+function App() {
+  return (
+    <div>
+        <h1> hello </h1>
+        <User />
+        <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,24 @@
+import About from './About';
+
+function User(){
+	return(
+			<>
+			<h1> hello user.....</h1>
+			<h2> hello user2</h2>
+			<About />
+			<p> hello user</p>
+			</>
+		);
+}
+
+function User2(){
+	return(
+			<>
+			
+			<h2> hello user2</h2>
+			<About />
+			
+			</>
+		);
+}
+export {User, User2};
